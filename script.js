86499d283e6c417394a48786f11e06152e2a0245
NormundsB <92847485+NormundsB@users.noreply.github.com>
2022-03-18 11:55:07 +0200

    KPETAIS

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 let zina = document.querySelector('.manaZina');
 let zinas = document.querySelector('.chataZinas');
 
-function sutitZinu()
-{
+function sutitZinu() {
     console.log('sutitZinu() darbojas');
 
     zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
 }
 
-async function ieladetChataZinas()
-{
+async function ieladetChataZinas() {
     let datiNoServera = await fetch('chatazinas.txt');
     let dati = await datiNoServera.text();
     zinas.innerHTML = dati;
 }
 
+setInterval(ieladetChataZinas, 1000)
