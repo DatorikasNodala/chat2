bbb5f708a317553a214df3f257496a8daa72a5a1
NormundsB <92847485+NormundsB@users.noreply.github.com>
2022-03-18 13:03:08 +0200

    ss

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API= "https://chat2022.normundsb.repl.co"
+const API = "https://chat2022.normundsb.repl.co/"
 
 let zina = document.querySelector('.manaZina');
 let zinas = document.querySelector('.chataZinas');
@@ -10,7 +10,7 @@ function sutitZinu() {
 }
 
 async function ieladetChataZinas() {
-    let datiNoServera = await fetch(API +'/lasit');
+    let datiNoServera = await fetch(API + '/lasit');
     let dati = await datiNoServera.text();
     zinas.innerHTML = dati;
 }
